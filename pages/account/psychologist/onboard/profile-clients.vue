3e3b76af4122697a4a8d6ebc4877c9862fe8f02d
Amir <amir53674268@gmail.com>
2025-02-19 22:41:38 +0500

    fixes profile page

--- pages/account/psychologist/onboard/profile-clients.vue
+++ pages/account/psychologist/onboard/profile-clients.vue
@@ -1093,56 +1093,56 @@ store.getMySchedulePsychologist()
           });
           // END Menu
 
-          // START PopUp общее 
-          $('.popup__layer').on('click', function () {
-              $('.popup').removeClass('active');
-              $('html').removeClass('hidden');
-          });
-          $('.popup__close').on('click', function () {
-              $('.popup').removeClass('active');
-              $('html').removeClass('hidden');
-          });
-          // END PopUp общее 
+          // // START PopUp общее 
+          // $('.popup__layer').on('click', function () {
+          //     $('.popup').removeClass('active');
+          //     $('html').removeClass('hidden');
+          // });
+          // $('.popup__close').on('click', function () {
+          //     $('.popup').removeClass('active');
+          //     $('html').removeClass('hidden');
+          // });
+          // // END PopUp общее 
 
 
-          // START  PopUp при клике по доступному времени в календаре
-          $('.table__cell.available').on('click', function () {
-              $('.popup.available_time').addClass('active');
-              // $('html').addClass('hidden');
-          });
-          // END  PopUp при клике по доступному времени в календаре
+          // // START  PopUp при клике по доступному времени в календаре
+          // $('.table__cell.available').on('click', function () {
+          //     $('.popup.available_time').addClass('active');
+          //     // $('html').addClass('hidden');
+          // });
+          // // END  PopUp при клике по доступному времени в календаре
 
-          // START  PopUp при клике по доступному времени в календаре
-          $(".table__cell.chosen").on("click", function (e) {
-              e.stopPropagation();
-              var popup = $(".popup_2.chosen_time");
-              $(this).append(popup);
-              popup.addClass("active");
-          });
-          $(document).on("click", function () {
-              $(".popup_2.chosen_time").removeClass("active");
-          });
-          $(".popup_2.chosen_time").on("click", function (e) {
-              e.stopPropagation();
-          });
-          // END  PopUp при клике по доступному времени в календаре
+          // // START  PopUp при клике по доступному времени в календаре
+          // $(".table__cell.chosen").on("click", function (e) {
+          //     e.stopPropagation();
+          //     var popup = $(".popup_2.chosen_time");
+          //     $(this).append(popup);
+          //     popup.addClass("active");
+          // });
+          // $(document).on("click", function () {
+          //     $(".popup_2.chosen_time").removeClass("active");
+          // });
+          // $(".popup_2.chosen_time").on("click", function (e) {
+          //     e.stopPropagation();
+          // });
+          // // END  PopUp при клике по доступному времени в календаре
 
-          // START "PopUp при клике по кнопке "Перенести""
-          $('.chosen_time__btn-switch').on('click', function () {
-              $('.popup.reschedule_time').addClass('active');
-              // $('html').addClass('hidden');
-          });
-          $('.calendar-date__item').on('click', function () {
-              $('.calendar-date__item').removeClass('active');
-              $(this).addClass('active');
-          });
-          // END "PopUp при клике по кнопке "Перенести""
+          // // START "PopUp при клике по кнопке "Перенести""
+          // $('.chosen_time__btn-switch').on('click', function () {
+          //     $('.popup.reschedule_time').addClass('active');
+          //     // $('html').addClass('hidden');
+          // });
+          // $('.calendar-date__item').on('click', function () {
+          //     $('.calendar-date__item').removeClass('active');
+          //     $(this).addClass('active');
+          // });
+          // // END "PopUp при клике по кнопке "Перенести""
 
-          // START "PopUp при клике по кнопке "Отменить""
-          $('.chosen_time__btn-delete').on('click', function () {
-              $('.popup.delete_time').addClass('active');
-              // $('html').addClass('hidden');
-          });
+          // // START "PopUp при клике по кнопке "Отменить""
+          // $('.chosen_time__btn-delete').on('click', function () {
+          //     $('.popup.delete_time').addClass('active');
+          //     // $('html').addClass('hidden');
+          // });
           // END "PopUp при клике по кнопке "Отменить""
         });
     }
